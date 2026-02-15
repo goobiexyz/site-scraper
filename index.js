0a2c240c6f1dfae6197b0403af688663daf6cf91
goobie <daineesvang@gmail.com>
2026-02-15 09:43:01 -0600

    switched to 3rd party furaffinity API for latest art fetch

--- index.js
+++ index.js
@@ -2,54 +2,12 @@ import fetch from 'node-fetch'
 import * as cheerio from 'cheerio'
 import fs from 'fs/promises'
 import Parser from 'rss-parser'
+import { Login, Gallery } from 'furaffinity-api'
 
 const ngProfileURL = 'https://goobieng.newgrounds.com/'
 const youtubeRssUrl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UC-xSJRpIEmeNXj-em-psGtA'
 const saveDir = '/srv/site-scraper/latest-posts'
 let parser = new Parser()
-
-// --- GET THE ART ---
-
-// get link to latest art post on newgrounds
-async function getLatestArtLink() {
-    // get the html
-    let response = await fetch(ngProfileURL)
-    let body = await response.text()
-
-    // parse with cheerio into DOM object
-    const $ = cheerio.load(body)
-
-    // get the link and title
-    let artLinkElement =  $('#pod_type_5').find('.item-portalitem-art-small:not(:has(rated-m), :has(rated-a))').first()
-    let link = artLinkElement.attr('href')
-    let title = artLinkElement.attr('title')
-
-    if (link && title) {
-        return {
-            "link" : link,
-            "title": title
-        }
-    } else {
-        return null
-    }
-}
-
-
-// get the src url of the img
-async function getImgSrc(url) {
-    // get the html
-    let response = await fetch(url)
-    let body = await response.text()
-
-    // parse with cheerio into DOM object
-    const $ = cheerio.load(body)
-
-    // get the image src url
-    let imgSrc = $('.art-images').find('img').first().attr('src')
-
-    return imgSrc
-}
-
 
 
 // --- MAIN SHIZZZZ ---
@@ -66,15 +24,55 @@ async function savePostData(postData, filename) {
 }
 
 async function saveArtPost() {
-    let artPostData = await getLatestArtLink()
-    if (!artPostData) throw new Error("Error scraping artist profile")
-    
-    let imgSrc = await getImgSrc(artPostData.link)
-    if (!imgSrc) throw new Error("Error getting image src")
-    
-    artPostData.imgSrc = imgSrc
-    
-    await savePostData(artPostData, "latest-art.json")
+    // Login with provided cookies and fetch first page of gallery
+    Login('af77c04b-e1d5-4ac0-ad62-159d20bc5d85', '9ae6c256-53ae-41fb-be56-c89301f495a3')
+
+    let results
+    try {
+        results = await Gallery('GracieArt')
+    } catch (err) {
+        console.error('Error fetching FurAffinity gallery:', err)
+        throw err
+    }
+
+    if (!Array.isArray(results) || results.length === 0) {
+        throw new Error('No gallery results found for GracieArt')
+    }
+
+    // Find first submission with a General rating (debug log ratings)
+    let artSubmission = null
+    for (const r of results) {
+        try {
+            const sub = await r.getSubmission()
+            const isGeneral = (typeof sub.rating === 'number' && sub.rating === 1) || (typeof sub.rating === 'string' && sub.rating.toLowerCase() === 'general')
+            if (sub && isGeneral) {
+                artSubmission = sub
+                break
+            }
+        } catch (e) {
+            // Skip entries that fail to load
+        }
+    }
+
+    if (!artSubmission) {
+        throw new Error('No general-rated submission found on first gallery page')
+    }
+
+    const artPostData = {
+        title: artSubmission.title || '',
+        link: artSubmission.url || (artSubmission.id ? ('https://www.furaffinity.net/view/' + artSubmission.id + '/') : '')
+    }
+
+    const imageCandidates = [
+        artSubmission.downloadUrl,
+        artSubmission.previewUrl
+    ].filter(Boolean)
+
+    if (imageCandidates.length > 0) {
+        artPostData.imgSrc = imageCandidates[0]
+    }
+
+    await savePostData(artPostData, 'latest-art.json')
 }
 
 async function saveVidPost() {
